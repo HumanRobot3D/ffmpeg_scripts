001c894280d48f248e6ad8175117b71ade2dc0ce
AB <ab@visuary.fr>
2023-05-21 16:44:23 +0200

    init

--- ffmpeg_speedup_video.py
+++ ffmpeg_speedup_video.py
@@ -5,7 +5,7 @@ import os
 input_file = r"G:\My Drive\WORK\3D_Work\Ants_youtube\renders\map3_trails.mp4"
 speed_factor = 5
 
-output_name = os.path.basename(input_file).split(".")[0] + "_" + str(speed_factor) + "xspeed" + ".mp4"
+output_name = os.path.basename(input_file).split(".")[0] + "_" + str(speed_factor) + "x_speed" + ".mp4"
 ouput_file = os.path.join(os.path.dirname(input_file),output_name)
 stream = ffmpeg.input(input_file)
 stream = ffmpeg.output(stream, ouput_file, vf=f"setpts={1/speed_factor}*PTS")
